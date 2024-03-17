e033540d0496760cbef20080f3e315a297e6abdb
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2024-03-17 23:00:45 +0000

    refactor: added coming soon

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,11 +1,8 @@
+import ComingSoon from './ComingSoon'
 import type { Component } from 'solid-js'
 
 const SettingsPage: Component = () => {
-    return (
-        <div class="flex flex-col items-center justify-center h-screen">
-            <h1 class="text-5xl font-bold">Tasks</h1>
-        </div>
-    )
+    return <ComingSoon />
 }
 
 export default SettingsPage
